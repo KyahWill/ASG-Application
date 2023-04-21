34c329b46dd4301ad9bd43fcf05e5e84b609478d
KyahWill <knownbutnotloved@gmail.com>
2023-04-22 03:01:13 +0800

    resolved backend more or less

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,11 +2,13 @@
   import { Router, Route } from 'svelte-routing'
   import Home from './pages/Home.svelte'
   import Details from './pages/Details.svelte'
+  import Chat from './pages/Chat.svelte';
 </script>
 
 <Router>
   <Route component={Home} />
   <Route path="/product/:id" let:params>
-      <Details id={params.id} />
+      <Details />
   </Route>
+  <Route path="/chat" component={Chat}/>
 </Router>
